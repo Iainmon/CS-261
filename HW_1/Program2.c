70fba1909b323b9086743fe42eb076222cc344aa
Iain Moncrief <Iainmoncrief@gmail.com>
2021-01-06 22:37:21 -1000

    program 2 done but needs comments

--- HW_1/Program2.c
+++ HW_1/Program2.c
@@ -1,12 +1,14 @@
 /* CS261- HW1 - Program2.c*/
 /* Name: Iain Moncrief
  * Date: January 6, 2021
- * Solution description:
+ * Solution description: 
  */
  
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+
+#define STUDENT_COUNT 10
 
 struct student{
 	char initials[2];
@@ -15,15 +17,24 @@ struct student{
 
 struct student* allocate(){
      /*Allocate memory for ten students*/
-     
+     struct student* students_head = (struct student*)malloc(sizeof(struct student) * 10);
+
      /*return the pointer*/
+     return students_head;
 }
+
 
 void generate(struct student* students){
      /*Generate random initials and scores for ten students.
 	The two initial letters must be capital and must be between A and Z. 
 	The scores must be between 0 and 100*/
-     
+     int i;
+     for (i = 0; i < STUDENT_COUNT; i += 1) {
+          students[i].initials[0] = 'A' + rand() % 26;
+          students[i].initials[1] = 'A' + rand() % 26;
+
+          students[i].score = rand() % 101;
+     }
 }
 
 void output(struct student* students){
@@ -32,29 +43,59 @@ void output(struct student* students){
               2. Initials  Score
               ...
               10. Initials Score*/
+     int i;
+     for (i = 0; i < STUDENT_COUNT; i += 1) {
+          printf("%i. %s %i\n", i, students[i].initials, students[i].score);
+     }
 }
 
 void summary(struct student* students){
      /*Compute and print the minimum, maximum and average scores of the ten students*/
-     
+
+     int min, max = 0;
+     int total, i, curr;
+     total = 0;
+     for (i = 0; i < STUDENT_COUNT; i += 1) {
+          curr = students[i].score;
+          total += curr;
+          if (curr < min) {
+               min = curr;
+               continue;
+          }
+          if (curr > max) {
+               max = curr;
+          }
+     }
+     int average;
+     average = (float)(total) / (float)(STUDENT_COUNT);
+
+     printf("min: %i, max: %i, average: %i \n", min, max, average);
 }
 
 void deallocate(struct student* stud){
      /*Deallocate memory from stud*/
+
+     free(stud);
+     stud = NULL;
 }
 
 int main(){
     struct student* stud = NULL;
     
     /*call allocate*/
-    
+    stud = allocate();
+
     /*call generate*/
+    generate(stud);
     
     /*call output*/
-    
+    output(stud);
+
     /*call summary*/
+    summary(stud);
     
     /*call deallocate*/
+    deallocate(stud);
 
     return 0;
 }
