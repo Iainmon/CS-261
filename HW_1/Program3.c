b3597971c3ca9c8594711ebd53a3155188599de7
Iain Moncrief <Iainmoncrief@gmail.com>
2021-01-07 12:53:29 -1000

    Homework 1 done

--- HW_1/Program3.c
+++ HW_1/Program3.c
@@ -1,27 +1,63 @@
 /* CS261- HW1 - Program3.c*/
-/* Name:
- * Date:
+/* Name: Iain Moncrief
+ * Date: January 7, 2021
  * Solution description:
  */
  
 #include <stdio.h>
+#include <stdlib.h>
+
+void swap(int* x, int* y) {
+    int temp = *x;
+    *x = *y;
+    *y = temp;
+}
 
 void sort(int* number, int n){
      /*Sort the array of integeres of length n*/     
+    int i, j, swapped;
+    for (i = 0; i < n-1; i += 1) {
+        swapped = 0;
+        for (j = 0; j < n - i - 1; j += 1) {
+            if (number[j] > number[j+1]) {
+                swap(&number[j], &number[j+1]);
+                swapped = 1;
+            }
+        }
+        if (swapped < 1) {
+            break;
+        }
+    }
 }
+
+
 
 int main(){
     /*Declare an integer n and assign it a value of 20.*/
+    int n;
+    n = 20;
     
     /*Allocate memory for an array of n integers using malloc.*/
+    int *numbers = (int*)malloc(sizeof(int) * n);
     
     /*Fill this array with random numbers, using rand().*/
-    
+    int i;
+    for (i = 0; i < n; i += 1) {
+        numbers[i] = rand();
+    }
+
     /*Print the contents of the array.*/
+    for (i = 0; i < n; i += 1) {
+        printf("numbers[%d] = %d\n", i, numbers[i]);
+    }
 
     /*Pass this array along with n to the sort() function of part a.*/
-    
+    sort(numbers, n);
+
     /*Print the contents of the array.*/    
-    
+    for (i = 0; i < n; i += 1) {
+        printf("numbers[%d] = %d\n", i, numbers[i]);
+    }
+
     return 0;
 }
--- HW_1/Program4.c
+++ HW_1/Program4.c
@@ -1,6 +1,6 @@
 /* CS261- HW1 - Program4.c*/
-/* Name:
- * Date:
+/* Name: Iain Moncrief
+ * Date: January 7, 2021
  * Solution description:
  */
  
@@ -12,22 +12,78 @@ struct student{
 	int score;
 };
 
+int compare_equal_strings_recurse(char *a, char *b, int len, int n) {
+    if (len - n >= len) {
+        return -1;
+    }
+    if (a[len - n] == b[len - n]) {
+        return compare_equal_strings_recurse(a, b, len, n-1);
+    }
+    if (a[len - n] < b[len - n]) {
+        return 1;
+    }
+    return 0;
+}
+
+
+/* Proposition function that a is less than b, or that a should come before b alphabetically. */
+int compare_equal_strings(char *a, char *b, int len) {
+    return compare_equal_strings_recurse(a, b, len, len);
+}
+
+void swap(struct student* a, struct student* b) {
+    struct student temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
 void sort(struct student* students, int n){
      /*Sort n students based on their initials*/     
+    int i, j, swapped;
+    for (i = 0; i < n-1; i += 1) {
+        swapped = 0;
+        for (j = 0; j < n - i - 1; j += 1) {
+            
+            if (compare_equal_strings(students[j+1].initials, students[j].initials, 2) > 0) {
+                swap(&students[j], &students[j+1]);
+                swapped = 1;
+            }
+        }
+        if (swapped < 1) {
+            break;
+        }
+    }
 }
 
 int main(){
     /*Declare an integer n and assign it a value.*/
-    
+    int n;
+    n = 20;
+
     /*Allocate memory for n students using malloc.*/
-    
+    struct student* students = (struct student*)malloc(sizeof(struct student) * n);
+
     /*Generate random IDs and scores for the n students, using rand().*/
-    
+    int i;
+    for (i = 0; i < n; i += 1) {
+        students[i].initials[0] = 'A' + rand() % 26;
+        students[i].initials[1] = 'A' + rand() % 26;
+
+        students[i].score = rand() % 101;
+    }
+
     /*Print the contents of the array of n students.*/
+    for (i = 0; i < n; i += 1) {
+        printf("%i. %s %i\n", i, students[i].initials, students[i].score);
+    }
 
     /*Pass this array along with n to the sort() function*/
-    
+    sort(students, n);
+
     /*Print the contents of the array of n students.*/
-    
+    for (i = 0; i < n; i += 1) {
+        printf("%i. %s %i\n", i, students[i].initials, students[i].score);
+    }
+
     return 0;
 }
